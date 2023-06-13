d6aafc53058c170e62b89a36a954343460635b58
David Poole <dpoole34@gatech.edu>
2023-06-13 11:16:01 -0400

    Fix in-core DiskDFJK with SCREENING=NONE

--- tests/pytests/test_comprehensive_jk_screening.py
+++ tests/pytests/test_comprehensive_jk_screening.py
@@ -70,7 +70,6 @@ def test_comprehensive_jk_screening(scf_type, scf_subtype, screening):
     #== other combinations error out badly and need to be fixed; skip them here ==#
     should_error_out = False
     should_error_out = should_error_out or (scf_type == "DFDIRJ+LINK" and screening in [ "SCHWARZ", "CSAM" ])
-    should_error_out = should_error_out or (scf_type == "DISK_DF" and scf_subtype != "OUT_OF_CORE" and screening == "NONE")
       
     E = 0.0 
     
