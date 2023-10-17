ca61e04218604b6dd9fe17c8fd35c9dca442ad5a
David Poole <dpoole34@gatech.edu>
2023-10-17 14:07:49 -0400

    Change called function

--- psi4/driver/procrouting/proc.py
+++ psi4/driver/procrouting/proc.py
@@ -4425,7 +4425,7 @@ def run_dlpnoccsd(name, **kwargs):
     # try GPU code
     if extras.addons("gpu_dlpno_ccsd"):
       import gpu_dlpno_ccsd
-      gpu_dlpno_ccsd.run_gpu_dlpno_ccsd("David")
+      gpu_dlpno_ccsd.say_hi()
     else:
       raise ValidationError("gpu_dlpno_ccsd plugin is not implemented correctly!")
 
