cad1f295f5f6565316da37b43244f77876112dfc
David Poole <dpoole34@atl1-1-01-013-38-1.pace.gatech.edu>
2024-01-11 13:47:37 -0500

    Start implementation of J_ref_

--- psi4/src/psi4/fisapt/fisapt.h
+++ psi4/src/psi4/fisapt/fisapt.h
@@ -63,6 +63,7 @@ class FISAPT {
     /// Global JK object
     std::shared_ptr<JK> jk_;
     std::shared_ptr<JK> jk_df_;
+    std::shared_ptr<JK> jk_ref_;
 
     /// Map of scalars
     std::map<std::string, double> scalars_;
